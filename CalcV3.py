0bfbc05ffadebea9efc1619ca35350b4b9267add
vivo <146621450+vivolev647@users.noreply.github.com>
2026-06-10 12:39:28 +0100

    fixed ur bad index variable names
    convention for loop indexes is to name them i, then go down the alphabet as you nest
    
    in lua, it's either _ or v i dont remember, but in most languages its i, then j, and so on.

--- CalcV3.py
+++ CalcV3.py
@@ -10,28 +10,29 @@ operator_location_table = []
 for q in range(len(inp)): # Loop that adds things into a table
     table.append(inp[q])
 
+
 operator_plus_amount = table.count("+")
 operator_minus_amount = table.count("-")
 operator_multiply_amount = table.count("*")
 operator_divide_amount = table.count("/")
 
 def operator_plus_amount_function():
-    for b in range(operator_plus_amount):
+    for i in range(operator_plus_amount):
         operator_location_table.append(table.index("+"))
         table[table.index("+")] = "."
 
 def operator_minus_amount_function():
-    for b in range(operator_minus_amount):
+    for i in range(operator_minus_amount):
         operator_location_table.append(table.index("-"))
         table[table.index("-")] = "."
 
 def operator_multiply_amount_function():
-    for b in range(operator_multiply_amount):
+    for i in range(operator_multiply_amount):
         operator_location_table.append(table.index("*"))
         table[table.index("*")] = "."
 
 def operator_divide_amount_function():
-    for b in range(operator_divide_amount):
+    for i in range(operator_divide_amount):
         operator_location_table.append(table.index("/"))
         table[table.index("/")] = "."
 operator_divide_amount_function()
@@ -42,7 +43,7 @@ operator_multiply_amount_function()
 operator_location_table.sort()
 print(operator_location_table)
 
-for w in range(len(operator_location_table)):
+for i in range(len(operator_location_table)):
     operator = inp[operator_location_table[w]]
     if result == 0:
         operand_one = inp[0:operator_location_table[w]]
